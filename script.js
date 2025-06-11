2545f9ac46296b5a3bbb19039e3ebbbeeae86180
Caio Emanuel <00001157521411sp@al.educacao.sp.gov.br>
2025-06-11 20:26:09 -0300

    def

--- script.js
+++
@@ -1,8 +0,0 @@
-function solicitarContato(modelo) {
-    const nome = prompt(`Você está interessado no ${modelo}. Informe seu nome para contato:`);
-    if (nome) {
-        alert(`Obrigado, ${nome}! Em breve entraremos em contato sobre o ${modelo}.`);
-    } else {
-        alert("Por favor, informe seu nome.");
-    }
-}
